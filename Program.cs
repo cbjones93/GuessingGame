bd1b16042c8efa9abc1f168d83e87dc84831c888
Cody_Jones <cbjones93@gmail.com>
2021-05-25 11:32:09 -0500

    phase 3 complete

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+main();
+void main()
+{
+    int numberOfGuesses = 0;
+    int secretNumber = 42;
+    while (numberOfGuesses < 4)
+    {
+
+        Console.WriteLine("What is the secret number?");
+        Console.WriteLine("-------------------------------------");
+        string userGuess = Console.ReadLine();
+
+
+        void Guess()
+        {
+            if (secretNumber == int.Parse(userGuess))
+            {
+                Console.WriteLine("WOW! You're a mindreader!");
+
+
+            }
+            else
+            {
+                Console.WriteLine("NOPE, Try again!");
+                numberOfGuesses++;
+            }
+            if (numberOfGuesses == 4)
+            {
+                Console.Clear();
+                Console.WriteLine("You're out of guesses! Prepare for doom!");
+
+            }
+        }
+        Guess();
+    }
+}
